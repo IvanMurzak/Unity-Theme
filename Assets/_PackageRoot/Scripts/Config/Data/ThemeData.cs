cb46c47e89b773cea69b6b8d5a3a9c486a705883
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2022-03-11 18:53:48 +0200

    Changed Theme interface

--- Assets/_PackageRoot/Scripts/Config/Data/ThemeData.cs
+++ Assets/_PackageRoot/Scripts/Config/Data/ThemeData.cs
@@ -8,16 +8,16 @@ namespace Unity.Theme
     [Serializable]
     public class ThemeData
     {
-        [TableColumnWidth(1), HideLabel]
+        [TableColumnWidth(1), HideLabel, InlineButton("Remove", "REMOVE")]
         public string themeName = "New Theme";
+
+        [GUIColor(1, 0.2f, 0.2f, 1)]
+        void Remove() => ThemeDatabaseInitializer.Config.RemoveTheme(this);
 
         [HideReferenceObjectPicker, HideLabel]
         [TableList(AlwaysExpanded = true, NumberOfItemsPerPage = 20, IsReadOnly = true, HideToolbar = true)]
         public List<ColorData> colors = new List<ColorData>();
 
         public ColorData GetColorByGuid(string guid) => string.IsNullOrEmpty(guid) ? null : colors?.FirstOrDefault(x => x.guid == guid);
-
-        [GUIColor(1, 0.2f, 0.2f, 1)]
-        [Button, LabelText("REMOVE")] void Remove() => ThemeDatabaseInitializer.Config.RemoveTheme(this);
     }
 }
