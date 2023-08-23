a9fabd53717b0a372a5e4baa2540640484753d92
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2023-08-23 13:32:31 -0700

    Update ThemeData.cs

--- Assets/_PackageRoot/Scripts/Config/Data/ThemeData.cs
+++ Assets/_PackageRoot/Scripts/Config/Data/ThemeData.cs
@@ -1,21 +1,13 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using Sirenix.OdinInspector;
+using System.Collections.Generic;
 
 namespace Unity.Theme
 {
     [Serializable]
     public class ThemeData
     {
-        [TableColumnWidth(1), HideLabel, InlineButton("Remove", "REMOVE")]
         public string themeName = "New Theme";
-
-        [GUIColor(1, 0.2f, 0.2f, 1)]
-        internal void Remove() => ThemeDatabaseInitializer.Config.RemoveTheme(this);
-
-        [HideReferenceObjectPicker, HideLabel]
-        [TableList(AlwaysExpanded = true, NumberOfItemsPerPage = 20, IsReadOnly = true, HideToolbar = true)]
         public List<ColorData> colors = new List<ColorData>();
 
         public ColorData GetColorByGuid(string guid) => string.IsNullOrEmpty(guid) ? null : colors?.FirstOrDefault(x => x.guid == guid);
