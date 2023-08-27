a0d0df40cee6128ac9c8d2e8cbb549e582b852e4
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2023-08-27 00:14:58 -0700

    Fixed Theme instance creation at package import

--- Assets/_PackageRoot/Editor/Scripts/ThemeInitializer.cs
+++ Assets/_PackageRoot/Editor/Scripts/ThemeInitializer.cs
@@ -1,7 +1,5 @@
 using System.Collections;
-using System.IO;
 using UnityEditor;
-using UnityEngine;
 
 namespace Unity.Theme.Editor
 {
@@ -11,120 +9,29 @@ namespace Unity.Theme.Editor
         public static IEnumerator Init()
         {
             yield return null; // let's Unity initialize itself and project resources first
-            var config = GetOrCreateConfig();
+            var config = Theme.Instance;
             ThemeWindowEditor.ShowWindow();
         }
 
         [MenuItem("Edit/Unity-Theme/Reset Default Palettes")]
         public static void ResetDefaultPalettes()
         {
-            var config = GetOrCreateConfig();
+            var config = Theme.Instance;
             
             config.RemoveAllThemes();
             config.RemoveAllColors();
 
-            SetDefaultPalettes(config);
+            config.SetDefaultPalettes();
 
             ThemeWindowEditor.ShowWindow().Invalidate();
         }
         [MenuItem("Edit/Unity-Theme/Set Default Palettes")]
         public static void SetDefaultPalettes()
         {
-            var config = GetOrCreateConfig();
-            SetDefaultPalettes(config);
+            var config = Theme.Instance;
+            config.SetDefaultPalettes();
 
             ThemeWindowEditor.ShowWindow().Invalidate();
-        }
-
-        public static Theme GetOrCreateConfig()
-        {
-            var config = Resources.Load<Theme>(Theme.PATH_FOR_RESOURCES_LOAD);
-            if (config == null)
-            {
-                Debug.Log($"<color=orange><b>Creating Unity-Theme database file</b> at <i>{Theme.PATH}</i></color>");
-                config = ScriptableObject.CreateInstance<Theme>();
-
-                SetDefaultPalettes(config);
-
-                var directory = Path.GetDirectoryName(Theme.PATH);
-                if (!Directory.Exists(directory))
-                {
-                    Directory.CreateDirectory(directory);
-                }
-
-                AssetDatabase.CreateAsset(config, Theme.PATH);
-                AssetDatabase.SaveAssets();
-            }
-            return config;
-        }
-
-        public static void SetDefaultPalettes(Theme config)
-        {
-            config.SetOrAddTheme("Light", true);
-
-            config.SetOrAddColor("Primary",                  "#6750A4");
-            config.SetOrAddColor("On Primary",               "#FFFFFF");
-            config.SetOrAddColor("Primary Container",        "#EADDFF");
-            config.SetOrAddColor("On Primary Container",     "#21005D");
-            
-            config.SetOrAddColor("Secondary",                "#625B71");
-            config.SetOrAddColor("On Secondary",             "#FFFFFF");
-            config.SetOrAddColor("Secondary Container",      "#E8DEF8");
-            config.SetOrAddColor("On Secondary Container",   "#1D192B");
-
-            config.SetOrAddColor("Tertiary",                 "#7D5260");
-            config.SetOrAddColor("On Tertiary",              "#FFFFFF");
-            config.SetOrAddColor("Tertiary Container",       "#FFD8E4");
-            config.SetOrAddColor("On Tertiary Container",    "#31111D");
-
-            config.SetOrAddColor("Error",                    "#B3261E");
-            config.SetOrAddColor("On Error",                 "#FFFFFF");
-            config.SetOrAddColor("Error Container",          "#F9DEDC");
-            config.SetOrAddColor("On Error Container",       "#410E0B");
-            
-            config.SetOrAddColor("Background",               "#FFFBFE");
-            config.SetOrAddColor("On Background",            "#1C1B1F");
-            config.SetOrAddColor("Surface",                  "#FFFBFE");
-            config.SetOrAddColor("On Surface",               "#1C1B1F");
-            
-            config.SetOrAddColor("Outline",                  "#79747E");
-            config.SetOrAddColor("Surface-Variant",          "#E7E0EC");
-            config.SetOrAddColor("On Surface-Variant",       "#49454F");
-
-            // ---------------------------------------------------------
-
-            config.SetOrAddTheme("Dark", true);
-
-            config.SetOrAddColor("Primary",                  "#D0BCFF");
-            config.SetOrAddColor("On Primary",               "#381E72");
-            config.SetOrAddColor("Primary Container",        "#4F378B");
-            config.SetOrAddColor("On Primary Container",     "#EADDFF");
-            
-            config.SetOrAddColor("Secondary",                "#CCC2DC");
-            config.SetOrAddColor("On Secondary",             "#332D41");
-            config.SetOrAddColor("Secondary Container",      "#4A4458");
-            config.SetOrAddColor("On Secondary Container",   "#E8DEF8");
-
-            config.SetOrAddColor("Tertiary",                 "#EFB8C8");
-            config.SetOrAddColor("On Tertiary",              "#492532");
-            config.SetOrAddColor("Tertiary Container",       "#633B48");
-            config.SetOrAddColor("On Tertiary Container",    "#FFD8E4");
-
-            config.SetOrAddColor("Error",                    "#F2B8B5");
-            config.SetOrAddColor("On Error",                 "#601410");
-            config.SetOrAddColor("Error Container",          "#8C1D18");
-            config.SetOrAddColor("On Error Container",       "#F9DEDC");
-            
-            config.SetOrAddColor("Background",               "#1C1B1F");
-            config.SetOrAddColor("On Background",            "#E6E1E5");
-            config.SetOrAddColor("Surface",                  "#1C1B1F");
-            config.SetOrAddColor("On Surface",               "#E6E1E5");
-            
-            config.SetOrAddColor("Outline",                  "#938F99");
-            config.SetOrAddColor("Surface-Variant",          "#49454F");
-            config.SetOrAddColor("On Surface-Variant",       "#CAC4D0");
-            
-            EditorUtility.SetDirty(config);
         }
     }
 }
---
+++ Assets/_PackageRoot/Scripts/Config/Theme.Editor.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+
+namespace Unity.Theme
+{
+#pragma warning disable CA2235 // Mark all non-serializable fields
+    public partial class Theme : ScriptableObject
+    {
+        public const string PATH                    = "Assets/Resources/Unity-Theme Database.asset";
+        public const string PATH_FOR_RESOURCES_LOAD = "Unity-Theme Database";
+
+        public static Theme GetOrCreateInstance()
+        {
+            var config = Resources.Load<Theme>(PATH_FOR_RESOURCES_LOAD);
+            if (config == null)
+            {
+#if UNITY_EDITOR
+                Debug.Log($"<color=orange><b>Creating Unity-Theme database file</b> at <i>{PATH}</i></color>");
+                config = ScriptableObject.CreateInstance<Theme>();
+
+                config.SetDefaultPalettes();
+
+                var directory = System.IO.Path.GetDirectoryName(PATH);
+                if (!System.IO.Directory.Exists(directory))
+                {
+                    System.IO.Directory.CreateDirectory(directory);
+                }
+
+                UnityEditor.AssetDatabase.CreateAsset(config, PATH);
+                UnityEditor.AssetDatabase.SaveAssets();
+
+                return config;
+#else
+                Debug.LogError($"Can't find <b>Unity-Theme database file</b> at <i>{PATH}</i>");
+#endif
+            }
+            return config;
+        }
+
+        public void SetDefaultPalettes()
+        {
+            SetOrAddTheme("Light", true);
+
+            SetOrAddColor("Primary",                  "#6750A4");
+            SetOrAddColor("On Primary",               "#FFFFFF");
+            SetOrAddColor("Primary Container",        "#EADDFF");
+            SetOrAddColor("On Primary Container",     "#21005D");
+            
+            SetOrAddColor("Secondary",                "#625B71");
+            SetOrAddColor("On Secondary",             "#FFFFFF");
+            SetOrAddColor("Secondary Container",      "#E8DEF8");
+            SetOrAddColor("On Secondary Container",   "#1D192B");
+
+            SetOrAddColor("Tertiary",                 "#7D5260");
+            SetOrAddColor("On Tertiary",              "#FFFFFF");
+            SetOrAddColor("Tertiary Container",       "#FFD8E4");
+            SetOrAddColor("On Tertiary Container",    "#31111D");
+
+            SetOrAddColor("Error",                    "#B3261E");
+            SetOrAddColor("On Error",                 "#FFFFFF");
+            SetOrAddColor("Error Container",          "#F9DEDC");
+            SetOrAddColor("On Error Container",       "#410E0B");
+            
+            SetOrAddColor("Background",               "#FFFBFE");
+            SetOrAddColor("On Background",            "#1C1B1F");
+            SetOrAddColor("Surface",                  "#FFFBFE");
+            SetOrAddColor("On Surface",               "#1C1B1F");
+            
+            SetOrAddColor("Outline",                  "#79747E");
+            SetOrAddColor("Surface-Variant",          "#E7E0EC");
+            SetOrAddColor("On Surface-Variant",       "#49454F");
+
+            // ---------------------------------------------------------
+
+            SetOrAddTheme("Dark", true);
+
+            SetOrAddColor("Primary",                  "#D0BCFF");
+            SetOrAddColor("On Primary",               "#381E72");
+            SetOrAddColor("Primary Container",        "#4F378B");
+            SetOrAddColor("On Primary Container",     "#EADDFF");
+            
+            SetOrAddColor("Secondary",                "#CCC2DC");
+            SetOrAddColor("On Secondary",             "#332D41");
+            SetOrAddColor("Secondary Container",      "#4A4458");
+            SetOrAddColor("On Secondary Container",   "#E8DEF8");
+
+            SetOrAddColor("Tertiary",                 "#EFB8C8");
+            SetOrAddColor("On Tertiary",              "#492532");
+            SetOrAddColor("Tertiary Container",       "#633B48");
+            SetOrAddColor("On Tertiary Container",    "#FFD8E4");
+
+            SetOrAddColor("Error",                    "#F2B8B5");
+            SetOrAddColor("On Error",                 "#601410");
+            SetOrAddColor("Error Container",          "#8C1D18");
+            SetOrAddColor("On Error Container",       "#F9DEDC");
+            
+            SetOrAddColor("Background",               "#1C1B1F");
+            SetOrAddColor("On Background",            "#E6E1E5");
+            SetOrAddColor("Surface",                  "#1C1B1F");
+            SetOrAddColor("On Surface",               "#E6E1E5");
+            
+            SetOrAddColor("Outline",                  "#938F99");
+            SetOrAddColor("Surface-Variant",          "#49454F");
+            SetOrAddColor("On Surface-Variant",       "#CAC4D0");
+
+#if UNITY_EDITOR
+            UnityEditor.EditorUtility.SetDirty(this);
+#endif
+        }
+    }
+
+#pragma warning restore CA2235 // Mark all non-serializable fields
+}
