801a4f90453f8c19b1331c092b8f1269c8b2de51
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-01-21 22:01:37 -0800

    Update Theme.Editor.cs

--- Assets/_PackageRoot/Scripts/Config/Theme.Editor.cs
+++ Assets/_PackageRoot/Scripts/Config/Theme.Editor.cs
@@ -53,67 +53,67 @@ namespace Unity.Theme
         {
             SetOrAddTheme("Light", true);
 
-            SetOrAddColor("Primary",                  "#6750A4");
-            SetOrAddColor("On Primary",               "#FFFFFF");
-            SetOrAddColor("Primary Container",        "#EADDFF");
-            SetOrAddColor("On Primary Container",     "#21005D");
-            
-            SetOrAddColor("Secondary",                "#625B71");
-            SetOrAddColor("On Secondary",             "#FFFFFF");
-            SetOrAddColor("Secondary Container",      "#E8DEF8");
-            SetOrAddColor("On Secondary Container",   "#1D192B");
+            SetOrAddColor("Primary",                    "#6750A4");
+            SetOrAddColor("Primary Text",               "#FFFFFF");
+            SetOrAddColor("Primary Container",          "#EADDFF");
+            SetOrAddColor("Primary Container Text",     "#21005D");
 
-            SetOrAddColor("Tertiary",                 "#7D5260");
-            SetOrAddColor("On Tertiary",              "#FFFFFF");
-            SetOrAddColor("Tertiary Container",       "#FFD8E4");
-            SetOrAddColor("On Tertiary Container",    "#31111D");
+            SetOrAddColor("Secondary",                  "#625B71");
+            SetOrAddColor("Secondary Text",             "#FFFFFF");
+            SetOrAddColor("Secondary Container",        "#E8DEF8");
+            SetOrAddColor("Secondary Container Text",   "#1D192B");
 
-            SetOrAddColor("Error",                    "#B3261E");
-            SetOrAddColor("On Error",                 "#FFFFFF");
-            SetOrAddColor("Error Container",          "#F9DEDC");
-            SetOrAddColor("On Error Container",       "#410E0B");
-            
-            SetOrAddColor("Background",               "#FFFBFE");
-            SetOrAddColor("On Background",            "#1C1B1F");
-            SetOrAddColor("Surface",                  "#FFFBFE");
-            SetOrAddColor("On Surface",               "#1C1B1F");
-            
-            SetOrAddColor("Outline",                  "#79747E");
-            SetOrAddColor("Surface-Variant",          "#E7E0EC");
-            SetOrAddColor("On Surface-Variant",       "#49454F");
+            SetOrAddColor("Tertiary",                   "#7D5260");
+            SetOrAddColor("Tertiary Text",              "#FFFFFF");
+            SetOrAddColor("Tertiary Container",         "#FFD8E4");
+            SetOrAddColor("Tertiary Container Text",    "#31111D");
+
+            SetOrAddColor("Error",                      "#B3261E");
+            SetOrAddColor("Error Text",                 "#FFFFFF");
+            SetOrAddColor("Error Container",            "#F9DEDC");
+            SetOrAddColor("Error Container Text",       "#410E0B");
+
+            SetOrAddColor("Background",                 "#FFFBFE");
+            SetOrAddColor("Background Text",            "#1C1B1F");
+            SetOrAddColor("Surface",                    "#FFFBFE");
+            SetOrAddColor("Surface Text",               "#1C1B1F");
+
+            SetOrAddColor("Outline",                    "#79747E");
+            SetOrAddColor("Surface-Variant",            "#E7E0EC");
+            SetOrAddColor("Surface-Variant Text",       "#49454F");
 
             // ---------------------------------------------------------
 
             SetOrAddTheme("Dark", true);
 
-            SetOrAddColor("Primary",                  "#D0BCFF");
-            SetOrAddColor("On Primary",               "#381E72");
-            SetOrAddColor("Primary Container",        "#4F378B");
-            SetOrAddColor("On Primary Container",     "#EADDFF");
-            
-            SetOrAddColor("Secondary",                "#CCC2DC");
-            SetOrAddColor("On Secondary",             "#332D41");
-            SetOrAddColor("Secondary Container",      "#4A4458");
-            SetOrAddColor("On Secondary Container",   "#E8DEF8");
+            SetOrAddColor("Primary",                    "#D0BCFF");
+            SetOrAddColor("Primary Text",               "#381E72");
+            SetOrAddColor("Primary Container",          "#4F378B");
+            SetOrAddColor("Primary Container Text",     "#EADDFF");
 
-            SetOrAddColor("Tertiary",                 "#EFB8C8");
-            SetOrAddColor("On Tertiary",              "#492532");
-            SetOrAddColor("Tertiary Container",       "#633B48");
-            SetOrAddColor("On Tertiary Container",    "#FFD8E4");
+            SetOrAddColor("Secondary",                  "#CCC2DC");
+            SetOrAddColor("Secondary Text",             "#332D41");
+            SetOrAddColor("Secondary Container",        "#4A4458");
+            SetOrAddColor("Secondary Container Text",   "#E8DEF8");
 
-            SetOrAddColor("Error",                    "#F2B8B5");
-            SetOrAddColor("On Error",                 "#601410");
-            SetOrAddColor("Error Container",          "#8C1D18");
-            SetOrAddColor("On Error Container",       "#F9DEDC");
-            
-            SetOrAddColor("Background",               "#1C1B1F");
-            SetOrAddColor("On Background",            "#E6E1E5");
-            SetOrAddColor("Surface",                  "#1C1B1F");
-            SetOrAddColor("On Surface",               "#E6E1E5");
-            
-            SetOrAddColor("Outline",                  "#938F99");
-            SetOrAddColor("Surface-Variant",          "#49454F");
-            SetOrAddColor("On Surface-Variant",       "#CAC4D0");
+            SetOrAddColor("Tertiary",                   "#EFB8C8");
+            SetOrAddColor("Tertiary Text",              "#492532");
+            SetOrAddColor("Tertiary Container",         "#633B48");
+            SetOrAddColor("Tertiary Container Text",    "#FFD8E4");
+
+            SetOrAddColor("Error",                      "#F2B8B5");
+            SetOrAddColor("Error Text",                 "#601410");
+            SetOrAddColor("Error Container",            "#8C1D18");
+            SetOrAddColor("Error Container Text",       "#F9DEDC");
+
+            SetOrAddColor("Background",                 "#1C1B1F");
+            SetOrAddColor("Background Text",            "#E6E1E5");
+            SetOrAddColor("Surface",                    "#1C1B1F");
+            SetOrAddColor("Surface Text",               "#E6E1E5");
+
+            SetOrAddColor("Outline",                    "#938F99");
+            SetOrAddColor("Surface-Variant",            "#49454F");
+            SetOrAddColor("Surface-Variant Text",       "#CAC4D0");
 
             Save();
         }
@@ -130,7 +130,12 @@ namespace Unity.Theme
 
                 var json = JsonUtility.ToJson(this, true);
                 File.WriteAllText(AssetsFilePath, json);
-                UnityEditor.EditorUtility.SetDirty(AssetFile);
+
+                var assetFile = AssetFile;
+                if (assetFile != null)
+                    UnityEditor.EditorUtility.SetDirty(assetFile);
+                else
+                    UnityEditor.AssetDatabase.Refresh();
             }
             catch (Exception e)
             {
