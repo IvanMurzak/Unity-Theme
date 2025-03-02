08a98c87b707590db91112d07e893b560dd92c4a
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-03-02 10:46:10 -0800

    Added more tests

--- Assets/_PackageRoot/Tests/Editor/Theme/TestTheme.Color.cs
+++ Assets/_PackageRoot/Tests/Editor/Theme/TestTheme.Color.cs
@@ -8,11 +8,20 @@ namespace Unity.Theme.Tests
 {
     public partial class TestTheme : TestBase
     {
-        [UnityTest] public IEnumerator SetColor_NoLogs() => TestUtils.RunNoLogs(SetColor);
-        [UnityTest] public IEnumerator SetColor()
+        [UnityTest] public IEnumerator SetColor_Undefined_NoLogs() => TestUtils.RunNoLogs(SetColor_Undefined);
+        [UnityTest] public IEnumerator SetColor_Undefined()
         {
             LogAssert.Expect(LogType.Error, $"[Theme] SetColor error. Color with name '{TestUtils.C_Color.Name_Undefined}' not found");
             Theme.Instance.SetColor(TestUtils.C_Color.Name_Undefined, Color.red);
+            yield return null;
+        }
+        [UnityTest] public IEnumerator SetColor_NoLogs() => TestUtils.RunNoLogs(SetColor);
+        [UnityTest] public IEnumerator SetColor()
+        {
+            var color = Color.cyan;
+            Theme.Instance.CurrentThemeName = TestUtils.C_Theme1.Name;
+            Theme.Instance.SetColor(TestUtils.C_Color.Name1, color);
+            Assert.AreEqual(color, Theme.Instance.GetColorByName(TestUtils.C_Color.Name1).Color);
             yield return null;
         }
     }
