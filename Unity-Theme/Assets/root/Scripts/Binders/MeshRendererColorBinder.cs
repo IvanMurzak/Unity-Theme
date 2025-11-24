9aa603cbc53557ca51ee0a4713942bc14f64ac7c
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-11-24 01:30:23 -0800

    Add MeshRendererColorBinder to bind theme colors to a MeshRenderer's material color

---
+++ Unity-Theme/Assets/root/Scripts/Binders/MeshRendererColorBinder.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Unity.Theme.Binders
+{
+    /// <summary>
+    /// Binds theme colors to a MeshRenderer's material color
+    /// Supports binding the main material color property or a custom shader property
+    /// </summary>
+    [AddComponentMenu("Theme/MeshRenderer Color Binder")]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class MeshRendererColorBinder : RendererColorBinder
+    {
+    }
+}
