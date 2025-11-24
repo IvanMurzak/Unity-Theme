5d93adce6ddf38f65173aefff8358b69bd98fe22
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-11-24 01:49:06 -0800

    Update TextMeshProColorBinder to use TMP_Text and adjust MCP connection settings

--- Unity-Theme/Assets/root/Scripts/Binders/TextMeshProColorBinder.cs
+++ Unity-Theme/Assets/root/Scripts/Binders/TextMeshProColorBinder.cs
@@ -4,12 +4,12 @@ using TMPro;
 namespace Unity.Theme.Binders
 {
     [AddComponentMenu("Theme/TextMeshPro Color Binder")]
-    public class TextMeshProColorBinder : GenericColorBinder<TextMeshProUGUI>
+    public class TextMeshProColorBinder : GenericColorBinder<TMP_Text>
     {
-        protected override void SetColor(TextMeshProUGUI target, Color color)
+        protected override void SetColor(TMP_Text target, Color color)
             => target.color = color;
 
-        protected override Color? GetColor(TextMeshProUGUI target)
+        protected override Color? GetColor(TMP_Text target)
             => target.color;
     }
 }
